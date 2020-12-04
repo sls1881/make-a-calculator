ac936f6b695f6f3b5e6d4278ec28360e179657ce
‘Shelby <sls1881@gmail.com>
2020-12-04 15:04:17 -0800

    Subtracting calculator funtion

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// import functions and grab DOM elements
+// add calculator variables
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-button');
 const addResult = document.getElementById('add-result');
 
-// initialize state
+//subtract calculator variables 
+const subtractInput1 = document.getElementById('subtract-input-1');
+const subtractInput2 = document.getElementById('subtract-input-2');
+const subtractButton = document.getElementById('s-button');
+const subtractResult = document.getElementById('subtract-result');
 
-// set event listeners to update state and DOM
+// set event listeners, add
 addButton.addEventListener('click', () => {
 
 const number1 = Number(addInput1.value);
@@ -15,3 +19,14 @@ const result = number1 + number2;
 
 addResult.textContent = result;
 });
+
+// set event listeners, subtract
+subtractButton.addEventListener('click', () => {
+
+    const number3 = Number(subtractInput1.value);
+    const number4 = Number(subtractInput2.value);
+    const sResult = number3 - number4;
+
+    subtractResult.textContent = sResult;
+});
+
