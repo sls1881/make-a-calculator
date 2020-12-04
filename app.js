b6184c21b1815a7efebd3de0dc1ede7678afe451
‘Shelby <sls1881@gmail.com>
2020-12-04 15:16:38 -0800

    Divide calculator function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// set variables 
+
 // add calculator variables
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
@@ -15,6 +17,14 @@ const multipleInput1 = document.getElementById('multiple-input-1');
 const multipleInput2 = document.getElementById('multiple-input-2');
 const multipleButton = document.getElementById('multiple-button');
 const multipleResult = document.getElementById('multiple-result');
+
+//divide calculator variables
+const divideInput1 = document.getElementById('divide-input-1');
+const divideInput2 = document.getElementById('divide-input-2');
+const divideButton = document.getElementById('divide-button');
+const divideResult = document.getElementById('divide-result');
+
+// set event listeners
 
 // set event listeners, add
 addButton.addEventListener('click', () => {
@@ -46,3 +56,12 @@ const mResult = number5 * number6;
 multipleResult.textContent = mResult;
 });
 
+// set event listeners, divide
+divideButton.addEventListener('click', () => {
+
+    const number7 = Number(divideInput1.value);
+    const number8 = Number(divideInput2.value);
+    const dResult = number7 / number8;
+    
+    divideResult.textContent = dResult;
+    });
